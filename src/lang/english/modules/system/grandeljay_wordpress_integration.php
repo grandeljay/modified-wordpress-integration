118eca11dc4e50b6d1d8672973b054a3f4121d3d
grandeljay <github@grandels.email>
2024-10-19 11:26:05 +0200

    fix(localisation): missing translations

--- src/lang/english/modules/system/grandeljay_wordpress_integration.php
+++ src/lang/english/modules/system/grandeljay_wordpress_integration.php
@@ -8,4 +8,6 @@ $translations = new Translations(__FILE__, Constants::MODULE_NAME);
 $translations->add('TITLE', 'grandeljay - WordPress Integration');
 $translations->add('TEXT_TITLE', 'WordPress Integration');
 
+$translations->add('BUTTON_POSTS_VIEW_ALL', 'View all posts');
+
 $translations->define();
