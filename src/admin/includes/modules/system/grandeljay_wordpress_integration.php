4fdf142c609c501531d701a3d19a89cccb45d8f2
grandeljay <github@grandels.email>
2025-01-27 15:13:07 +0100

    chore(module): update to v1.2.0

--- src/admin/includes/modules/system/grandeljay_wordpress_integration.php
+++ src/admin/includes/modules/system/grandeljay_wordpress_integration.php
@@ -16,7 +16,7 @@ use RobinTheHood\ModifiedStdModule\Classes\StdModule;
 
 class grandeljay_wordpress_integration extends StdModule
 {
-    public const VERSION = '1.1.0';
+    public const VERSION = '1.2.0';
 
     public function __construct()
     {
